90cef0936e0e0ca9517c79447585bb5e8def9a22
Mario Visic <mario@mariovisic.com>
2017-12-24 12:51:32 +0800

    Add sinata auto reload, get strava login working

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
+gem 'sinatra-contrib'
 gem 'strava-api-v3'
 gem 'colorize'
 gem 'supervised_learning'
--- web/app.rb
+++ web/app.rb
@@ -2,12 +2,18 @@ require 'bundler'
 Bundler.setup
 
 require 'dotenv/load'
+require 'strava/api/v3'
 require 'sinatra'
+require 'sinatra/reloader' if development?
 
 get '/' do
   erb :index, locals: { strava_redirect_uri: URI.join(ENV['STRAVA_REDIRECT_DOMAIN'], '/login').to_s, strava_client_id:  ENV['STRAVA_CLIENT_ID'] }
 end
 
 get '/login' do
-  raise params.inspect
+  access_information = Strava::Api::V3::Auth.retrieve_access(ENV['STRAVA_CLIENT_ID'], ENV['STRAVA_CLIENT_SECRET'], params[:code])
+  access_token = access_information['access_token']
+  athlete_information = access_information['athlete']
+
+  raise athlete_information.inspect
 end
